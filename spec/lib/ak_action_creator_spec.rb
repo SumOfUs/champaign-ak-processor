807a91f3f9e8b0b7245fb73c566f36989edbfc27
Eric Boersma <eric.boersma@gmail.com>
2015-07-31 16:08:01 -0400

    Adding a test for AkActionCreator.

---
+++ spec/lib/ak_action_creator_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+require 'webmock/rspec'
+
+describe AkActionCreator do
+  let(:hostname) { 'http://localhost' }
+  let(:username) { 'fake_username' }
+  let(:password) { 'fake_password' }
+  let(:creator) { AkActionCreator.new hostname, username, password }
+
+  it 'should call the endpoint for creating an action in ActionKit' do
+    stub_request(:post, 'http://fake_username:fake_password@localhost/action/')
+
+    creator.create_action 'test_page', 'fake_email@sumofus.org'
+    expect(WebMock).to have_requested(:post, 'http://fake_username:fake_password@localhost/action/')
+  end
+
+end
