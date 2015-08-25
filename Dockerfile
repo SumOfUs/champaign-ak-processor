45e59be883fa9c2d53b293be8b4bd86d726a02c9
tuuli <tuuli.pollanen@gmail.com>
2015-08-24 17:10:55 -0700

    make Dockerrun into a template that gets tagged with current git commit sha when deploying

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,6 @@ RUN bundle install
 
 EXPOSE 3000
 ADD . /myapp
+WORKDIR /myapp
 
 CMD bundle exec rails s -b 0.0.0.0
--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,17 @@ docker push soutech/champaign-ak-processor:$SHA1
 # Prepare the source bundle .zip
 EB_BUCKET=champaign.dockerrun.files
 echo 'Shipping source bundle to S3...'
-zip -r9 $SHA1-config.zip Dockerrun.aws.json ./.ebextensions/
-SOURCE_BUNDLE=$SHA1-config.zip
+#sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 
+DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 aws configure set default.region $AWS_REGION
-aws s3 cp $SOURCE_BUNDLE s3://$EB_BUCKET/$SOURCE_BUNDLE
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 
 # Ship to AWS Elastic Beanstalk
 echo 'Creating new application version...'
 aws elasticbeanstalk create-application-version --application-name 'Champaign core application' \
-  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$SOURCE_BUNDLE
+  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 echo 'Updating environment...'
 aws elasticbeanstalk update-environment --environment-name 'champaign-ak-processor' \
     --version-label $SHA1
+
