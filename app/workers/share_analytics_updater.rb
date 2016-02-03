18d703283a7ec03b6f4fade99d3ac74b22e1d843
tuuli <tuuli.pollanen@gmail.com>
2016-02-03 11:35:59 -0800

    Move share progress update code from rake task in Champaign onto the worker

--- app/workers/share_analytics_updater.rb
+++ app/workers/share_analytics_updater.rb
@@ -1,7 +1,18 @@
+require "net/http"
+require "uri"
+
 class ShareAnalyticsUpdater
-  include HTTParty
-  def self.dispatch_update
-    response = HTTParty.get("#{champaign_url}/api/update_share_analytics")
-    render nothing: true, status: response.code
+  def self.update_shares
+    puts "Fetching data..."
+
+    Share::Button.all.each do |button|
+      uri = 'http://run.shareprogress.org/api/v1/buttons/analytics'
+      uri = URI.parse(uri)
+
+      response = Net::HTTP.post_form(uri, {key: Settings.share_progress_api_key, id: button.sp_id})
+      button.update(analytics: response.body )
+    end
+
+    puts "Fetching has completed."
   end
 end
