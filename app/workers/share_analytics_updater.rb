91333567c7a881df1890de6dc0ca7de29da0fa42
tuuli <tuuli.pollanen@gmail.com>
2016-02-03 14:23:24 -0800

    store JSON-parsed response body under analytics

--- app/workers/share_analytics_updater.rb
+++ app/workers/share_analytics_updater.rb
@@ -5,14 +5,14 @@ class ShareAnalyticsUpdater
   def self.update_shares
     puts "Fetching data..."
 
-    Share::Button.all.each do |button|
-      uri = 'http://run.shareprogress.org/api/v1/buttons/analytics'
-      uri = URI.parse(uri)
+    uri = 'http://run.shareprogress.org/api/v1/buttons/analytics'
+    uri = URI.parse(uri)
 
+    Share::Button.all.each do |button|
       response = Net::HTTP.post_form(uri, {key: ENV['SHARE_PROGRESS_API_KEY'], id: button.sp_id})
       parsed_body = JSON.parse(response.body).with_indifferent_access
       if parsed_body[:success]
-        button.update(analytics: response.body )
+        button.update!(analytics: response.body )
       else
         raise "ShareProgress button update failed with the following message from their API: #{parsed_body[:message]}."
       end
