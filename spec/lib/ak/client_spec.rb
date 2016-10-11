23e8d559d125116ece83e5ae32e86d7360186396
Rodrigo Pavano <rodrigopavano@gmail.com>
2016-10-11 16:54:25 -0300

    Survey response processing

--- spec/lib/ak/client_spec.rb
+++ spec/lib/ak/client_spec.rb
@@ -2,14 +2,8 @@ require 'rails_helper'
 
 describe Ak::Client do
   describe 'client' do
-    before do
-      allow(ENV).to receive(:[]).with("AK_USERNAME"){"foo"}
-      allow(ENV).to receive(:[]).with("AK_PASSWORD"){"bar"}
-      allow(ENV).to receive(:[]).with("AK_HOST"){"http://foo.com"}
-    end
-
     it 'instantiates with credentials' do
-      expect(Ak::Client.client.credentials).to eq({password: 'bar', username: 'foo'})
+      expect(Ak::Client.client.credentials).to eq({password: ENV['AK_PASSWORD'], username: ENV['AK_USERNAME']})
     end
   end
 end
