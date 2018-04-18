ec7d7bd132f557e4d2259d8eb804cb11f975741f
Omar Sahyoun <osahyoun@gmail.com>
2018-04-18 11:33:17 +0100

    Rescue from failed calls
    Should allow action to cont. being processed.

--- app/services/country_service.rb
+++ app/services/country_service.rb
@@ -3,7 +3,11 @@ class CountryService
     def extend_with_local_data(data)
       case data[:params][:country]
       when 'United Kingdom'
-        data[:params][:user_uk_parliamentary_constituency] = GetConstituency.for(data[:params][:postal])
+        begin
+          data[:params][:user_uk_parliamentary_constituency] = GetConstituency.for(data[:params][:postal])
+        rescue => e
+          Rails.logger.error "Failed to get constituency: #{e.message}"
+        end
       end
     end
   end
